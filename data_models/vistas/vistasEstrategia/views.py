18f5441a0e85b9fb19044669f30b7aeb848420a9
Osmel <akumanosame22@gmail.com>
2025-05-03 23:06:55 -0400

    agregando descargar a visualizar estrategia

--- data_models/vistas/vistasEstrategia/views.py
+++ data_models/vistas/vistasEstrategia/views.py
@@ -245,19 +245,23 @@ def estrategia_docx(request, id):
 
 
 def estrategia_pdf(request, estra_id):
-    estrategia = get_object_or_404(Estrategia, id=estra_id)
+    if request.user.is_authenticated:
+        estrategia = get_object_or_404(Estrategia, id=estra_id)
 
-    # Renderizar HTML
-    html = render_to_string("estrategia_pdf.html", {'estrategia': estrategia})
+        # Renderizar HTML
+        html = render_to_string("estrategia_pdf.html", {'estrategia': estrategia})
 
-    # Generar PDF desde HTML
-    buffer = BytesIO()
-    pisa_status = pisa.CreatePDF(html, dest=buffer)
-    buffer.seek(0)
+        # Generar PDF desde HTML
+        buffer = BytesIO()
+        pisa_status = pisa.CreatePDF(html, dest=buffer)
+        buffer.seek(0)
 
-    if pisa_status.err:
-        return HttpResponse("Error al generar el PDF", status=500)
+        if pisa_status.err:
+            return HttpResponse("Error al generar el PDF", status=500)
 
-    response = HttpResponse(buffer, content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="{estrategia.nombre}_{estrategia.id}.pdf"'
-    return response
+        response = HttpResponse(buffer, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{estrategia.nombre}_{estrategia.id}.pdf"'
+        return response
+    else:
+        messages.error(request, "No estás autenticado.")
+        return redirect("login")
